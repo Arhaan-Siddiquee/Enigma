24a77c645b919de87a1b088373b89064f6d21d41
Arhaan Siddiquee <siddiqueearhaan@gmail.com>
2025-02-22 23:12:04 +0530

    new

---
+++ liveblocks.config.ts
@@ -0,0 +1,67 @@
+import { LiveMap, createClient } from "@liveblocks/client";
+import { createRoomContext } from "@liveblocks/react";
+
+const client = createClient({
+  throttle: 16,
+  publicApiKey: process.env.NEXT_PUBLIC_LIVEBLOCKS_PUBLIC_KEY!,
+  async resolveUsers({ userIds }) {
+    // Used only for Comments. Return a list of user information retrieved
+    // from `userIds`. This info is used in comments, mentions etc.
+
+    return [];
+  },
+});
+
+
+type Presence = {};
+type Storage = {
+  canvasObjects: LiveMap<string, any>;
+};
+type UserMeta = {};
+type RoomEvent = {};
+export type ThreadMetadata = {
+  resolved: boolean;
+  zIndex: number;
+  time?: number;
+  x: number;
+  y: number;
+};
+
+export const {
+  suspense: {
+    RoomProvider,
+    useRoom,
+    useMyPresence,
+    useUpdateMyPresence,
+    useSelf,
+    useOthers,
+    useOthersMapped,
+    useOthersConnectionIds,
+    useOther,
+    useBroadcastEvent,
+    useEventListener,
+    useErrorListener,
+    useStorage,
+    useObject,
+    useMap,
+    useList,
+    useBatch,
+    useHistory,
+    useUndo,
+    useRedo,
+    useCanUndo,
+    useCanRedo,
+    useMutation,
+    useStatus,
+    useLostConnectionListener,
+    useThreads,
+    useUser,
+    useCreateThread,
+    useEditThreadMetadata,
+    useCreateComment,
+    useEditComment,
+    useDeleteComment,
+    useAddReaction,
+    useRemoveReaction,
+  },
+} = createRoomContext<Presence, Storage, UserMeta, RoomEvent, ThreadMetadata>(client);
