995c4d3b5086d3c93491d5319a1913a8b7136a7d
Arhaan Siddiquee <siddiqueearhaan@gmail.com>
2025-02-23 00:08:03 +0530

    commit

--- figma_clone-main/app/layout.tsx
+++ figma_clone-main/app/layout.tsx
@@ -6,9 +6,9 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import Room from "./Room";
 
 export const metadata = {
-  title: "Figma Clone",
+  title: "Enigma",
   description:
-    "A minimalist Figma clone using fabric.js and Liveblocks for realtime collaboration",
+    "A minimalist Editor for Small Needs",
 };
 
 const workSans = Work_Sans({
